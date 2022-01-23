e1cef1b56cfbf27c94a04ab307a0ea347b0a5b11
cjhendrick42 <cjhendrick42@tntech.edu>
2022-01-23 16:18:28 -0600

    Add zip code and station ID search functionality

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StationsComponent } from './stations/stations.component';
+import { DataComponent } from './data/data.component';
+import { DisplayComponent } from './display/display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    StationsComponent,
+    DataComponent,
+    DisplayComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
