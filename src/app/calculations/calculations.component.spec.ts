1d02ea6ace1e1b6719f9942ff4c6d6df3c20b09d
Brian Kemp <bskemp01@gmail.com>
2022-04-07 20:57:51 -0500

    added calculations component

---
+++ src/app/calculations/calculations.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalculationsComponent } from './calculations.component';
+
+describe('CalculationsComponent', () => {
+  let component: CalculationsComponent;
+  let fixture: ComponentFixture<CalculationsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CalculationsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CalculationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
